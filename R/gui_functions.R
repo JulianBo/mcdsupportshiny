142a57433e10f075e48a901c512f945d11ede8c3
Bothe <Julian.bothe@geographie.uni-muenchen.de>
2018-12-05 16:07:06 +0100

    Zwischenstand Implementierung dynamische GUI und Breaking

--- R/gui_functions.R
+++ R/gui_functions.R
@@ -18,13 +18,16 @@
 #'                 extra pages by name will be added, right now this is not possible.
 #' @param mainpageposition One of "first", "last", "none".
 #'                         Should main page be first or last or be omitted?
-#' @param reusingvalues Should old slider values be used? Needed when creating sliderGuis on
-#'                      several pages, and Sliders should be set to old values again.
+#' @param reusingvalues Should old slider values be used? If yes, provide list of values to be accessed by name
+#'                      Usually this is done by \code{isolate(input)} or a variation thereof. Values which are not
+#'                      in this list will be set to the default value.
+#'                      Needed when creating sliderGuis on several pages, and Sliders should be set to old values again.
 #' @param parents_name Label used to name root when creating fold-out-panel for root.
 #' @param minweight Standard minimum slider weight.
 #' @param maxweight Standard maximum slider weight.
 #' @param standardweight Standard slider weight.
 #' @param open.maxdepth From which depth on to create fold-out-panels, if not declared otherwise in x.
+#'                      Maximum:12 (see \code{\link{shiny:column}}).Must be larger than breaking (if provided).
 #' @param cb_title Default description label for checkbox.
 #'
 #' @return Slidergui: List of Sliders (breaking== NULL or breaking==FALSE)
@@ -40,18 +43,29 @@ rSliderGui<-function(x,
                      open.maxdepth=Inf,
                      cb_title= "I don't know"
                      ){
-  recSliderGui(x,depth=0,
-               breaking=breaking,mainpageposition=mainpageposition,
-               reusingvalues = reusingvalues,
-               parents_name = parents_name, minweight = minweight,
-               maxweight = maxweight, standardweight = standardweight,
-               open.maxdepth = open.maxdepth, cb_title=cb_title)
+
+
+  stopifnot(is.list(x))
+
+  if (is.null(breaking)){
+    breaking <-0
+  } else  if (is.logical(breaking)){breaking<-as.integer(breaking)
+  } else  stopifnot(breaking>=0)
+
+
+  slGui <- recSliderGui(x,depth=0,
+                        breaking=breaking,
+                        reusingvalues = reusingvalues,
+                        parents_name = parents_name, minweight = minweight,
+                        maxweight = maxweight, standardweight = standardweight,
+                        open.maxdepth = open.maxdepth, cb_title=cb_title)
+  slGui
 }
 
 #' Internal Version of rSliderGui
 #'
 #' @param x
-#' @param depth actual depth
+#' @param depth actual depth - root is 0
 #' @param breaking
 #' @param mainpageposition
 #' @param reusingvalues
@@ -63,32 +77,37 @@ rSliderGui<-function(x,
 #' @param cb_title
 #'
 #'
-#' @return
+#' @return a list of rows as UIoutput - Sliders and collapsebars.
 
 recSliderGui<-function(x, depth=0,
-                       breaking=NULL, mainpageposition=c("first","last", "none"),
+                       breaking=0,
                        reusingvalues=!is.null(breaking),
                        parents_name="genauer",
                      minweight=0,maxweight=100, standardweight=30,
                      open.maxdepth=Inf, cb_title= "I don't know"
 ){
-  # Ziel: Elemente für innerhalb sidebarPanel zurückliefern.
+  # Ziel: Elemente für innerhalb sidebarPanel bzw. für ganze Seite zurückliefern.
 
   #Erste Ebene: Offen
-  #Zweite Ebene: Eingerückt. Offen.
+  #Zweite Ebene: Eingerückt. Offen. (ggf Korrigiert um breaking)
   # Danach: CollapsePanel.
 
-  stopifnot(depth>=0)
-  stopifnot(is.list(x))
+  #Alle Sliders und Collapsepanels für sich als tagList (das meint: alle fluidrows, und collapsePanelGroups)
+  #Dann  jeweils als Geschachtelte Liste.
 
-  #Ebene, ab wo mit Ausklapppanels gearbeitet wird. Kann nur kleiner werden, nie größer
+
+  #Ebene, ab wo mit AusklappPanels gearbeitet wird. Kann nur kleiner werden, nie größer
   open.maxdepth<- getOpen.Maxdepth(x,open.maxdepth)
 
+  ##weitere TESTS sind bereits in rCollapsePanel
+  stopifnot(depth>=0)
+  stopifnot(open.maxdepth>=breaking)
 
 
-  ret <- tagList()
 
-  #Durch Elemente des aktuellen Knotens iterieren
+  ret <- list()
+
+  ##Durch Elemente des aktuellen Knotens iterieren
   for(i in 1:length(x) ){
     list.elem <- x[[i]]
     elem.name <- names(x)[i]
@@ -103,57 +122,89 @@ recSliderGui<-function(x, depth=0,
 
     #Falls Element. GGf. Child-Elemente parsen
     if("class" %in% names(list.elem)){
-      ## Hier Slider selbst
-      returnvalue <-tagList(sliderCheckboxInput(elem.name,
-                                        description = paste0(this.description, collapse=""),
-                                        min = this.minweight,
-                                        max = this.maxweight,
-                                        value = this.standardweight,
-                                        cb_title = cb_title)
-                            )
 
-      #Ggf. Rekursion
-      if(list.elem$class=="elements")
-        returnvalue <-tagList(returnvalue, recSliderGui(list.elem,depth+1,elem.name,
-                                                      minweight = this.minweight, maxweight=this.maxweight,
-                                                      standardweight = this.standardweight,
-                                                      open.maxdepth = open.maxdepth,
-                                                      cb_title = cb_title) )
-
+      ## Hier Slider selbst - Taglist!
+      ## In Abhängigkeit davon, ob alte Werte wiederbenutzt werden sollen oder nicht.
+      if (is.list(reusingvalues)) {
+        returnvalue <-tagList(sliderCheckboxInput(elem.name,
+                                                  description = paste0(this.description, collapse=""),
+                                                  min = this.minweight,
+                                                  max = this.maxweight,
+                                                  value =reusingvalues[elem.name] %||%this.standardweight,
+                                                  cb_title = cb_title)
+        )
+      } else {
+        returnvalue <-tagList(sliderCheckboxInput(elem.name,
+                                                  description = paste0(this.description, collapse=""),
+                                                  min = this.minweight,
+                                                  max = this.maxweight,
+                                                  value = this.standardweight,
+                                                  cb_title = cb_title)
+        )
+      }
 
       ##Je nach Tiefe zusammenfügen
+      ##TODO: HIER LIEGT FEHLER! Alles muss in einzelne Reihe gesetzt werden!
       # Falls noch offen. Je nach Tiefe eingerückt
       if (depth<open.maxdepth){
 
-        #zurückgeben
-        ret<- tagList(ret,
-                      fluidRow(column(width= 12-depth,
-                                      offset=depth,
-                                      returnvalue
+        ##Falls Mainpage
+        #Ganz normal einrücken
+        if (depth<=breaking) {
 
-                      ))
+          #zurückgeben
+          ret<- c(ret,
+                  indentedRow(indention = depth,
+                              returnvalue)
+          )
+
+          ##Falls keine Mainpage
+          #Einrückung um Tiefe des Breaks bereinigen
+        } else {
+          ret<- c(ret,
+                  indentedRow(indention = depth -breaking,
+                              returnvalue)
+          )
+
+        }
+
+        #Falls nicht mehr offen; nicht einrücken
+      } else  if (depth>= open.maxdepth){
+        ret<- c(ret,list(returnvalue) )
+      }#  if (depth>= open.maxdepth)
+
+
+      ##Ggf. REKURSION
+      ##Slider (Element selbst) mit evtl. Child-Knoten zusammensetzen. mit c(): sollte list() sein.
+      if(list.elem$class=="elements")
+        ret <-c(ret,
+                # tagList(
+                  recSliderGui(list.elem,depth+1,
+                                     breaking = breaking,
+                                     reusingvalues = reusingvalues,
+                                     elem.name,
+                                     minweight = this.minweight, maxweight=this.maxweight,
+                                     standardweight = this.standardweight,
+                                     open.maxdepth = open.maxdepth,
+                                     cb_title = cb_title)
+                # )
         )
 
-      }#if (depth<open.maxdepth)
 
-      #Falls nicht mehr offen; nicht einrücken
-      if (depth>= open.maxdepth){
-        ret<- tagList(ret,returnvalue)
-      }#  if (depth>= open.maxdepth)
+
 
     } # if "class" in names
 
   }#for
 
-  result <- ret
-
   #Falls nicht mehr offen, muss komplete Sliderliste in ein bsCollapsePanel gepackt werden
-  if (depth>= open.maxdepth)
+  if (depth>= open.maxdepth){
     result <- bsCollapse(id=paste0("bsc",parents_name,  collapse="_"),
                          bsCollapsePanel(title=paste0("Faktor ",parents_name," einstellen",  collapse=""), ##Hier beschreibung einstellen
-                                         ret
-                         )#bsCollapsePanel
-    )#bsCollapse
+                                         tagList(ret)
+                                         )#bsCollapsePanel
+                         )#bsCollapse
+  } else result <- ret
 
   return(result)
 
@@ -161,6 +212,28 @@ recSliderGui<-function(x, depth=0,
 
 }
 
+
+#' Creation of indented fluidrows
+#'
+#' Creates an indented fluidrow
+#'
+#' @param ... Elements to be included (each on several row).
+#' @param indention Integer between 0 and 11. Indention.
+#' @param tagListWrap Should fluidrow be wrapped into \code{\link{shiny:tagList}}.
+#'
+#' @return \code{\link{shiny:fluidRow}}/\code{\link{shiny:column}} with indented elements.
+#'
+#' @examples
+indentedRow<-function(...,indention=0, tagListWrap=FALSE){
+  stopifnot(indention>=0 & indention<12)
+  ret<-fluidRow(column(
+    width= 12-indention,
+    ...,
+    offset=indention
+  ))
+  if (tagListWrap) { return(tagList(ret))
+  } else return (ret)
+}
 
 
 #' Recursively turn Configurations list into a vector of colors
