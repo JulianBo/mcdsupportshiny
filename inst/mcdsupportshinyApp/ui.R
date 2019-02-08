a429f59e6f7bd3a5b86025d3ea3dd86608cd500f
Julian Bothe <julian.bothe@geographie.uni-muenchen.de>
2019-02-08 16:28:57 +0100

    Changed SliderCheckbox: Only one calculation of reactives. Disabled javascript-onclick. But still time-consuming: loading and calculation.

--- inst/mcdsupportshinyApp/ui.R
+++ inst/mcdsupportshinyApp/ui.R
@@ -17,7 +17,7 @@ library(mcdsupportshiny)
 #source("Setup.R", encoding="UTF-8") #local=FALSE, auch in ui.R sichtbar
 source("Setup_INOLA.R", encoding="UTF-8") #local=FALSE, auch in ui.R sichtbar
 
-slGui1<-rSliderGuiInput("slGui1",configList,breaking=1,
+slGui1<-rSliderGuiInput("slGui1",configList,breaking=0,
                         beschreibungs_text=texte$begruessungstext2,
                         title_text=TRUE,
                    cb_title="Ich weiß nicht")
